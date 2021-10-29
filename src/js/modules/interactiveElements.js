31a696ac6723c34e856d276154002f161c0a8649
Bowie <bowie.leidy@gmail.com>
2021-10-29 16:19:25 -0400

    built logic into webpack dist

--- src/js/modules/interactiveElements.js
+++ src/js/modules/interactiveElements.js
@@ -3,7 +3,7 @@ export const interactiveElements = ((a) => {
 
     let step = document.querySelector('.step-navigation-item.active span').innerText;
     let faqs = document.querySelectorAll('.accordion__item');
-    let slider = document.querySelectorAll('.image-slider__navigation*');
+    let slider = document.querySelectorAll('.image-slider__navigationb*');
 
     if(step) {
         arr.push(step);
