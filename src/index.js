90a7a6956758767aa5e56c02431569dbed627e24
Bowie <bowie.leidy@gmail.com>
2021-10-29 16:13:29 -0400

    adding logic to make sure there are elements before introducing them to the loop.

--- src/index.js
+++ src/index.js
@@ -9,28 +9,38 @@ let navLinks = document.querySelectorAll('.nav-link');
 let sections = Array.from(document.getElementsByTagName('section'));
 let anchors = Array.from(document.getElementsByTagName('a'));
 
-sections.forEach((section) => {
-    let sectionObj = structureSectionObject(section);
-    tagElementWithDataAttributes(section, sectionObj);
-});
+if(sections.length) {
+    sections.forEach((section) => {
+        let sectionObj = structureSectionObject(section);
+        tagElementWithDataAttributes(section, sectionObj);
+    });
+};
 
-buttons.forEach((button) => {
-    let btnObj = structureButtonObject(button);
-    tagElementWithDataAttributes(button, btnObj);
-});
+if(buttons.length) {
+    buttons.forEach((button) => {
+        let btnObj = structureButtonObject(button);
+        tagElementWithDataAttributes(button, btnObj);
+    });
+};
 
-navLinks.forEach((navLink) => {
-    let linkObj = structureLinkObject(navLink);
-    tagElementWithDataAttributes(navLink, linkObj);
-});
+if(navLinks.length) {
+    navLinks.forEach((navLink) => {
+        let linkObj = structureLinkObject(navLink);
+        tagElementWithDataAttributes(navLink, linkObj);
+    });
+};
 
-anchors.forEach((anchor) => {
-    let anchorObj = structureLinkObject(anchor);
-    tagElementWithDataAttributes(anchor, anchorObj);
-});
+if(anchors.length) {
+    anchors.forEach((anchor) => {
+        let anchorObj = structureLinkObject(anchor);
+        tagElementWithDataAttributes(anchor, anchorObj);
+    });
+};
 
-interactiveElements.forEach((anchor) => {
-    debugger;
-    let anchorObj = structureLinkObject(anchor);
-    tagElementWithDataAttributes(anchor, anchorObj);
-});
+if(interactiveElements.length) {
+    interactiveElements.forEach((anchor) => {
+        debugger;
+        let anchorObj = structureLinkObject(anchor);
+        tagElementWithDataAttributes(anchor, anchorObj);
+    });
+};
