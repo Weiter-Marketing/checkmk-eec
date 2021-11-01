86284d58b68aabd6697a4692e8f2247da42573bf
Bowie <bowie.leidy@gmail.com>
2021-11-01 17:56:42 -0400

    added fall back for step variable

--- src/js/modules/interactiveElements.js
+++ src/js/modules/interactiveElements.js
@@ -1,11 +1,11 @@
 export const interactiveElements = ((a) => {
     let arr = [];
 
-    let step = document.querySelector('.step-navigation-item.active span').innerText;
+    let step = (document.querySelector('.step-navigation-item.active span') !== undefined) ? document.querySelector('.step-navigation-item.active span').innerText : undefined;
     let faqs = document.querySelectorAll('.accordion__item');
     let slider = document.querySelectorAll('.image-slider__navigationb*');
 
-    if(step) {
+    if(step !== undefined) {
         arr.push(step);
     }
     if(faqs) {
